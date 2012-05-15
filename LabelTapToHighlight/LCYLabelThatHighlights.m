e25a2e6fff08222382dedcaa2559295162e886ab
Krishna Kotecha <krishna.kotecha@logiccolony.com>
2012-05-15 21:37:47 +0100

    Code cleanup

--- LabelTapToHighlight/LCYLabelThatHighlights.m
+++ LabelTapToHighlight/LCYLabelThatHighlights.m
@@ -123,14 +123,4 @@ static NSString *HIGHLIGHT_IMAGE_NAME = @"glow.png";
     return _highlightView;
 }
 
-
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
-{
-    // Drawing code
-}
-*/
-
 @end
